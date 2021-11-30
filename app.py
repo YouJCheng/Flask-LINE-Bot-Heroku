68b15a5af9a43701e12978c9a2f206e7ec7f3a03
YouJCheng <jiancheng06@gmail.com>
2021-11-30 14:26:07 +0800

    test reply sticker_message

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 import os
 from datetime import datetime
-
+# 利用 route 處理路由
 from flask import Flask, abort, request
 
+# 初始化LINT BOT
 # https://github.com/line/line-bot-sdk-python
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+
+# 利用 handler 處理 LINE 觸發事件
+from linebot.models import *
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -35,17 +38,19 @@ def callback():
 def handle_message(event):
 
     get_message = event.message.text
-
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
+    if 
     line_bot_api.reply_message(event.reply_token, reply)
 
 
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image(event):
     message_id = event.message.id
-    
-  # Send To Line
-    reply = TextSendMessage(text=f"{message_id}")
-    line_bot_api.reply_message(event.reply_token, reply)
+    # Send To Line
+    sticker_message = StickerSendMessage(
+        package_id='1',
+        sticker_id='1'
+    )
+    line_bot_api.reply_message(event.reply_token, sticker_message)
 
